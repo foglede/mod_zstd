fe6e934246477babfa66c1749aab915556ce4faa
foglede <hikcvg@gmail.com>
2025-02-09 05:03:13 +0800

    Update mod_zstd.c
    更改输出方式到 Mod_status，这样比较符合官方写法

--- mod_zstd.c
+++ mod_zstd.c
@@ -1,9 +1,11 @@
-/* 
+/*
 * 我查看RFC规则的时候，我暂时没有ZSTD到底能不能在HTTP1下开启
 * 大部分现代浏览器在HTTP1下都不发送ZSTD 头，这在一定程度上影响了我对该模块的测试
 * 2025年2月8日
-* https://github.com/foglede/mod_zstd
-*/
+* 2025-02-09
+* Http1 可以zstd
+* @author https://github.com/foglede/mod_zstd
+ */
 
 #include "httpd.h"
 #include "http_core.h"
@@ -11,6 +13,11 @@
 #include "apr_strings.h"
 #include "http_protocol.h"
 #include "http_config.h"
+#include "ap_hooks.h"        
+#include "ap_provider.h"     
+#include "apr_optional.h"    
+#include "apr_optional_hooks.h"
+#include "mod_status.h"
 
 #include <zstd.h>
 
@@ -28,29 +35,31 @@ typedef struct zstd_server_config_t {
     int window_size;
     int strategy;
     etag_mode_e etag_mode;
-    const char *note_ratio_name;
-    const char *note_input_name;
-    const char *note_output_name;
+    const char* note_ratio_name;
+    const char* note_input_name;
+    const char* note_output_name;
 } zstd_server_config_t;
 
-static void *create_server_config(apr_pool_t *p, server_rec *s)
+/***
+ * @see https://raw.githack.com/facebook/zstd/release/doc/zstd_manual.html#Chapter2
+ */
+static void* create_server_config(apr_pool_t* p, server_rec* s)
 {
-    zstd_server_config_t *conf = apr_pcalloc(p, sizeof(*conf));
+    zstd_server_config_t* conf = apr_pcalloc(p, sizeof(*conf));
 
-    /* 后续再改成从配置中读取 */
-    conf->compression_level = 7;  // 默认压缩级别，类似于 brotli 的 quality=5
+    conf->compression_level = 7;  // 默认
     conf->window_size = 128;
-    // https://raw.githack.com/facebook/zstd/release/doc/zstd_manual.html#Chapter2 
+
     conf->strategy = ZSTD_fast;
     conf->etag_mode = ETAG_MODE_ADDSUFFIX;
 
     return conf;
 }
 
-static const char *set_filter_note(cmd_parms *cmd, void *dummy,
-                                   const char *arg1, const char *arg2)
+static const char* set_filter_note(cmd_parms* cmd, void* dummy,
+    const char* arg1, const char* arg2)
 {
-    zstd_server_config_t *conf =
+    zstd_server_config_t* conf =
         ap_get_module_config(cmd->server->module_config, &zstd_module);
 
     if (!arg2) {
@@ -69,38 +78,38 @@ static const char *set_filter_note(cmd_parms *cmd, void *dummy,
     }
     else {
         return apr_psprintf(cmd->pool, "Unknown ZstdFilterNote type '%s'",
-                            arg1);
+            arg1);
     }
 
     return NULL;
 }
 
-static const char *set_compression_level(cmd_parms *cmd, void *dummy,
-                                         const char *arg)
+static const char* set_compression_level(cmd_parms* cmd, void* dummy,
+    const char* arg)
 {
-    zstd_server_config_t *conf =
+    zstd_server_config_t* conf =
         ap_get_module_config(cmd->server->module_config, &zstd_module);
     int val = atoi(arg);
 
     if (val < ZSTD_minCLevel() || val > ZSTD_maxCLevel()) {
         return apr_psprintf(cmd->pool, "ZstdCompressionLevel must be between %d and %d",
-                            ZSTD_minCLevel(), ZSTD_maxCLevel());
+            ZSTD_minCLevel(), ZSTD_maxCLevel());
     }
-
+    //最好不要中断 reutrn 直接 log warn 就行，但是得  v1.5.0+ 版本才行 使用 ZSTD_CLEVEL_DEFAULT  
     conf->compression_level = val;
     return NULL;
 }
 
-static const char *set_window_size(cmd_parms *cmd, void *dummy,
-                                   const char *arg)
+static const char* set_window_size(cmd_parms* cmd, void* dummy,
+    const char* arg)
 {
-    zstd_server_config_t *conf =
+    zstd_server_config_t* conf =
         ap_get_module_config(cmd->server->module_config, &zstd_module);
     int val = atoi(arg);
 
-    if (val < 0 || val > 128) {
-        return apr_psprintf(cmd->pool, "ZstdWindowSize must be between %d and %d",
-                            0, 128);
+    if (val < 0 || val > 101) {
+        return apr_psprintf(cmd->pool, "ZstdWindowSize = (ZSTD_c_windowLog) must be between %d and %d",
+            0, 101);
     }
 
 
@@ -108,10 +117,10 @@ static const char *set_window_size(cmd_parms *cmd, void *dummy,
     return NULL;
 }
 
-static const char *set_etag_mode(cmd_parms *cmd, void *dummy,
-                                 const char *arg)
+static const char* set_etag_mode(cmd_parms* cmd, void* dummy,
+    const char* arg)
 {
-    zstd_server_config_t *conf =
+    zstd_server_config_t* conf =
         ap_get_module_config(cmd->server->module_config, &zstd_module);
 
     if (ap_cstr_casecmp(arg, "AddSuffix") == 0) {
@@ -131,27 +140,27 @@ static const char *set_etag_mode(cmd_parms *cmd, void *dummy,
 }
 
 typedef struct zstd_ctx_t {
-    ZSTD_CCtx *cctx;
-    apr_bucket_brigade *bb;
+    ZSTD_CCtx* cctx;
+    apr_bucket_brigade* bb;
     apr_off_t total_in;
     apr_off_t total_out;
 } zstd_ctx_t;
 
-static apr_status_t cleanup_ctx(void *data)
+static apr_status_t cleanup_ctx(void* data)
 {
-    zstd_ctx_t *ctx = data;
+    zstd_ctx_t* ctx = data;
 
     ZSTD_freeCCtx(ctx->cctx);
     ctx->cctx = NULL;
     return APR_SUCCESS;
 }
 
-static zstd_ctx_t *create_ctx(int compression_level,
-                              int window_size,
-                              apr_bucket_alloc_t *alloc,
-                              apr_pool_t *pool)
+static zstd_ctx_t* create_ctx(int compression_level,
+    int window_size,
+    apr_bucket_alloc_t* alloc,
+    apr_pool_t* pool)
 {
-    zstd_ctx_t *ctx = apr_pcalloc(pool, sizeof(*ctx));
+    zstd_ctx_t* ctx = apr_pcalloc(pool, sizeof(*ctx));
 
     ctx->cctx = ZSTD_createCCtx();
     ZSTD_CCtx_setParameter(ctx->cctx, ZSTD_c_compressionLevel, compression_level);
@@ -165,29 +174,29 @@ static zstd_ctx_t *create_ctx(int compression_level,
     return ctx;
 }
 
-static apr_status_t process_chunk(zstd_ctx_t *ctx,
-                                  const void *data,
-                                  apr_size_t len,
-                                  ap_filter_t *f)
+static apr_status_t process_chunk(zstd_ctx_t* ctx,
+    const void* data,
+    apr_size_t len,
+    ap_filter_t* f)
 {
     ZSTD_inBuffer input = { data, len, 0 };
     size_t out_size = ZSTD_compressBound(len);
-    char *out_buffer = apr_palloc(f->r->pool, out_size);
+    char* out_buffer = apr_palloc(f->r->pool, out_size);
     ZSTD_outBuffer output = { out_buffer, out_size, 0 };
 
     size_t remaining = ZSTD_compressStream2(ctx->cctx, &output, &input, ZSTD_e_continue);
     if (ZSTD_isError(remaining)) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, f->r, APLOGNO(03459)
-                      "Error while compressing data: %s",
-                      ZSTD_getErrorName(remaining));
+            "Error while compressing data: %s",
+            ZSTD_getErrorName(remaining));
         return APR_EGENERAL;
     }
 
     if (output.pos > 0) {
-        apr_bucket *b = apr_bucket_heap_create(out_buffer, output.pos, NULL,
-                                              ctx->bb->bucket_alloc);
+        apr_bucket* b = apr_bucket_heap_create(out_buffer, output.pos, NULL,
+            ctx->bb->bucket_alloc);
         APR_BRIGADE_INSERT_TAIL(ctx->bb, b);
-        
+
         apr_status_t rv = ap_pass_brigade(f->next, ctx->bb);
         apr_brigade_cleanup(ctx->bb);
         if (rv != APR_SUCCESS) {
@@ -200,13 +209,13 @@ static apr_status_t process_chunk(zstd_ctx_t *ctx,
     return APR_SUCCESS;
 }
 
-static apr_status_t flush(zstd_ctx_t *ctx,
-                          ZSTD_EndDirective mode,
-                          ap_filter_t *f)
+static apr_status_t flush(zstd_ctx_t* ctx,
+    ZSTD_EndDirective mode,
+    ap_filter_t* f)
 {
     ZSTD_inBuffer input = { NULL, 0, 0 };
     size_t out_size = ZSTD_compressBound(ZSTD_CStreamInSize());
-    char *out_buffer = apr_palloc(f->r->pool, out_size);
+    char* out_buffer = apr_palloc(f->r->pool, out_size);
     ZSTD_outBuffer output = { out_buffer, out_size, 0 };
 
     size_t remaining;
@@ -214,14 +223,14 @@ static apr_status_t flush(zstd_ctx_t *ctx,
         remaining = ZSTD_compressStream2(ctx->cctx, &output, &input, mode);
         if (ZSTD_isError(remaining)) {
             ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, f->r, APLOGNO(03460)
-                          "Error while flushing data: %s",
-                          ZSTD_getErrorName(remaining));
+                "Error while flushing data: %s",
+                ZSTD_getErrorName(remaining));
             return APR_EGENERAL;
         }
 
         if (output.pos > 0) {
-            apr_bucket *b = apr_bucket_heap_create(out_buffer, output.pos, NULL,
-                                                  ctx->bb->bucket_alloc);
+            apr_bucket* b = apr_bucket_heap_create(out_buffer, output.pos, NULL,
+                ctx->bb->bucket_alloc);
             APR_BRIGADE_INSERT_TAIL(ctx->bb, b);
             ctx->total_out += output.pos;
         }
@@ -230,13 +239,13 @@ static apr_status_t flush(zstd_ctx_t *ctx,
     return APR_SUCCESS;
 }
 
-static const char *get_content_encoding(request_rec *r)
+static const char* get_content_encoding(request_rec* r)
 {
-    const char *encoding;
+    const char* encoding;
 
     encoding = apr_table_get(r->headers_out, "Content-Encoding");
     if (encoding) {
-        const char *err_enc;
+        const char* err_enc;
 
         err_enc = apr_table_get(r->err_headers_out, "Content-Encoding");
         if (err_enc) {
@@ -249,19 +258,19 @@ static const char *get_content_encoding(request_rec *r)
 
     if (r->content_encoding) {
         encoding = encoding ? apr_pstrcat(r->pool, encoding, ",",
-                                          r->content_encoding, NULL)
-                            : r->content_encoding;
+            r->content_encoding, NULL)
+            : r->content_encoding;
     }
 
     return encoding;
 }
 
-static apr_status_t compress_filter(ap_filter_t *f, apr_bucket_brigade *bb)
+static apr_status_t compress_filter(ap_filter_t* f, apr_bucket_brigade* bb)
 {
-    request_rec *r = f->r;
-    zstd_ctx_t *ctx = f->ctx;
+    request_rec* r = f->r;
+    zstd_ctx_t* ctx = f->ctx;
     apr_status_t rv;
-    zstd_server_config_t *conf;
+    zstd_server_config_t* conf;
 
     if (APR_BRIGADE_EMPTY(bb)) {
         return APR_SUCCESS;
@@ -270,10 +279,10 @@ static apr_status_t compress_filter(ap_filter_t *f, apr_bucket_brigade *bb)
     conf = ap_get_module_config(r->server->module_config, &zstd_module);
 
     if (!ctx) {
-        const char *encoding;
-        const char *token;
-        const char *accepts;
-        const char *q = NULL;
+        const char* encoding;
+        const char* token;
+        const char* accepts;
+        const char* q = NULL;
 
         if (r->main || r->status == HTTP_NO_CONTENT
             || apr_table_get(r->subprocess_env, "no-zstd")
@@ -285,11 +294,12 @@ static apr_status_t compress_filter(ap_filter_t *f, apr_bucket_brigade *bb)
         encoding = get_content_encoding(r);
 
         if (encoding) {
-            const char *tmp = encoding;
+            const char* tmp = encoding;
 
             token = ap_get_token(r->pool, &tmp, 0);
-            // 我认为没必要这个循环 ，AI也没理解这个while 在干啥？安全检查吗？
+            // 这个算是Apache 的私有协议了。。
             while (token && *token) {
+                //strcmp(token, "br")
                 if (strcmp(token, "identity") != 0 &&
                     strcmp(token, "7bit") != 0 &&
                     strcmp(token, "8bit") != 0 &&
@@ -341,13 +351,14 @@ static apr_status_t compress_filter(ap_filter_t *f, apr_bucket_brigade *bb)
 
         if (!encoding || ap_cstr_casecmp(encoding, "identity") == 0) {
             apr_table_setn(r->headers_out, "Content-Encoding", "zstd");
-        } else {
+        }
+        else {
             apr_table_mergen(r->headers_out, "Content-Encoding", "zstd");
         }
 
         if (r->content_encoding) {
             r->content_encoding = apr_table_get(r->headers_out,
-                                                "Content-Encoding");
+                "Content-Encoding");
         }
 
         apr_table_unset(r->headers_out, "Content-Length");
@@ -357,7 +368,7 @@ static apr_status_t compress_filter(ap_filter_t *f, apr_bucket_brigade *bb)
             apr_table_unset(r->headers_out, "ETag");
         }
         else if (conf->etag_mode == ETAG_MODE_ADDSUFFIX) {
-            const char *etag = apr_table_get(r->headers_out, "ETag");
+            const char* etag = apr_table_get(r->headers_out, "ETag");
 
             if (etag) {
                 apr_size_t len = strlen(etag);
@@ -376,12 +387,12 @@ static apr_status_t compress_filter(ap_filter_t *f, apr_bucket_brigade *bb)
         }
 
         ctx = create_ctx(conf->compression_level, conf->window_size,
-                         f->c->bucket_alloc, r->pool);
+            f->c->bucket_alloc, r->pool);
         f->ctx = ctx;
     }
 
     while (!APR_BRIGADE_EMPTY(bb)) {
-        apr_bucket *e = APR_BRIGADE_FIRST(bb);
+        apr_bucket* e = APR_BRIGADE_FIRST(bb);
 
         if (r->header_only && r->bytes_sent) {
             ap_remove_output_filter(f);
@@ -396,17 +407,17 @@ static apr_status_t compress_filter(ap_filter_t *f, apr_bucket_brigade *bb)
 
             if (conf->note_input_name) {
                 apr_table_setn(r->notes, conf->note_input_name,
-                               apr_off_t_toa(r->pool, ctx->total_in));
+                    apr_off_t_toa(r->pool, ctx->total_in));
             }
             if (conf->note_output_name) {
                 apr_table_setn(r->notes, conf->note_output_name,
-                               apr_off_t_toa(r->pool, ctx->total_out));
+                    apr_off_t_toa(r->pool, ctx->total_out));
             }
             if (conf->note_ratio_name) {
                 if (ctx->total_in > 0) {
-                    int ratio = (int) (ctx->total_out * 100 / ctx->total_in);
+                    int ratio = (int)(ctx->total_out * 100 / ctx->total_in);
                     apr_table_setn(r->notes, conf->note_ratio_name,
-                                   apr_itoa(r->pool, ratio));
+                        apr_itoa(r->pool, ratio));
                 }
                 else {
                     apr_table_setn(r->notes, conf->note_ratio_name, "-");
@@ -441,7 +452,7 @@ static apr_status_t compress_filter(ap_filter_t *f, apr_bucket_brigade *bb)
             APR_BRIGADE_INSERT_TAIL(ctx->bb, e);
         }
         else {
-            const char *data;
+            const char* data;
             apr_size_t len;
 
             rv = apr_bucket_read(e, &data, &len, APR_BLOCK_READ);
@@ -458,6 +469,7 @@ static apr_status_t compress_filter(ap_filter_t *f, apr_bucket_brigade *bb)
     return APR_SUCCESS;
 }
 
+/**
 static int zstd_status_handler(request_rec *r)
 {
     if (strcmp(r->handler, "zstd-status")) {
@@ -494,14 +506,41 @@ static int zstd_info_handler(request_rec *r)
 
     return OK;
 }
+**/
 
-static void register_hooks(apr_pool_t *p)
+/*
+ *  2025年2月9日 输出到 mod_status 要直观点
+ */
+static int zstd_status_hook(request_rec* r, int flags)
 {
-    ap_hook_handler(zstd_status_handler, NULL, NULL, APR_HOOK_MIDDLE);
-    ap_hook_handler(zstd_info_handler, NULL, NULL, APR_HOOK_MIDDLE);
-    ap_register_output_filter("ZSTD_COMPRESS", compress_filter, NULL,
-                              AP_FTYPE_CONTENT_SET);
+    if (!(flags & AP_STATUS_SHORT)) {
+        zstd_server_config_t* conf = ap_get_module_config(r->server->module_config, &zstd_module);
+
+        ap_rputs("<hr>\n", r);
+        ap_rputs("<h1>Zstd Module <cite> https://github.com/foglede/mod_zstd </cite></h1>\n", r);
+        ap_rputs("<h2>Zstd Module Configuration</h2>\n", r);
+        ap_rputs("<dl>\n", r);
+
+        ap_rprintf(r, "<dt>Zstd Library Version:</dt><dd>%s</dd>\n", ZSTD_versionString());
+        ap_rprintf(r, "<dt>ZstdCompressionLevel:</dt><dd>%d</dd>\n", conf->compression_level);
+        ap_rprintf(r, "<dt>ZstdWindowSize:</dt><dd>%d</dd>\n", conf->window_size);
+        ap_rprintf(r, "<dt>ZstdAlterETag:</dt><dd>%d</dd>\n", conf->etag_mode);
+
+        ap_rputs("</dl>\n", r);
+    }
+    return OK;
 }
+
+static void register_hooks(apr_pool_t* p)
+{
+    //ap_hook_handler(zstd_status_handler, NULL, NULL, APR_HOOK_MIDDLE);
+    //ap_hook_handler(zstd_info_handler, NULL, NULL, APR_HOOK_MIDDLE);
+    ap_register_output_filter("ZSTD_COMPRESS", compress_filter, NULL, AP_FTYPE_CONTENT_SET);
+
+    APR_OPTIONAL_HOOK(ap, status_hook, zstd_status_hook, NULL, NULL, APR_HOOK_MIDDLE);
+}
+
+
 
 static const command_rec zstd_config_cmds[] = {
     AP_INIT_TAKE12("ZstdFilterNote", set_filter_note,
@@ -513,8 +552,7 @@ static const command_rec zstd_config_cmds[] = {
                   "better compression but slower)"),
     AP_INIT_TAKE1("ZstdWindowSize", set_window_size,
                   NULL, RSRC_CONF,
-                  "Window size between min and max (larger windows can "
-                  "improve compression, but require more memory)"),
+                  "Maximum allowed back-reference distance, expressed as power of 2."),
     AP_INIT_TAKE1("ZstdAlterETag", set_etag_mode,
                   NULL, RSRC_CONF,
                   "Set how mod_zstd should modify ETag response headers: "
@@ -530,4 +568,4 @@ AP_DECLARE_MODULE(zstd) = {
     NULL,                      /* merge per-server config structures */
     zstd_config_cmds,          /* command apr_table_t */
     register_hooks             /* register hooks */
-}; 
+};

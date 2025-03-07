53834d997482827d5f97b0b9a130bc74c4ce8f98
foglede <hikcvg@gmail.com>
2025-03-08 03:05:41 +0800

    解决 issues/5 的问题

--- mod_zstd.h
+++ mod_zstd.h
@@ -1,5 +1,22 @@
-/*
-* @author https://github.com/foglede/mod_zstd
-* @date 2025-02-22
-*/
 #define MOD_ZSTD_VERSION "0.3"
+
+typedef enum {
+    ETAG_MODE_ADDSUFFIX = 0,
+    ETAG_MODE_NOCHANGE = 1,
+    ETAG_MODE_REMOVE = 2
+} etag_mode_e;
+	
+typedef struct zstd_server_config_t {
+    apr_int32_t compression_level,strategy,workers;
+    etag_mode_e etag_mode;
+    const char *note_ratio_name;
+    const char *note_input_name;
+    const char *note_output_name;
+} zstd_server_config_t;
+
+typedef struct zstd_ctx_t {
+    ZSTD_CCtx *cctx;
+    apr_bucket_brigade *bb;
+    apr_off_t total_in;
+    apr_off_t total_out;
+} zstd_ctx_t;
